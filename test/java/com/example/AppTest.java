c74b8f102b61b576ceaef06348ae606df121eeb8
bellore123 <hemabalaji.r2023@vitstudent.ac.in>
2026-04-15 17:39:28 +0530

    updated code

--- test/java/com/example/AppTest.java
+++ test/java/com/example/AppTest.java
@@ -7,8 +7,8 @@ public class App {
     }
 
     public static String calculateGrade(double average) {
-        if (average >= 90) return "A";
-        else if (average >= 75) return "B";
+        if (average >= 95) return "A";
+        else if (average >= 65) return "B";
         else if (average >= 60) return "C";
         else if (average >= 50) return "D";
         else return "F";
